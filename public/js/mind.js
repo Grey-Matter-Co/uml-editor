94b44016e361157aa46c1777230e44c7aa2382b5
jdann <juanddvalle20@gmail.com>
2021-11-27 03:26:59 -0600

    Changes Danny

--- public/js/mind.js
+++ public/js/mind.js
@@ -1,60 +1,4 @@
-let nodes = [];
-let selectedNode = null;
-let arcos = [];
-
-function getNodeAt(x, y, nodes) {
-	for (let index = 0; index < nodes.length; index++) {
-		const node = nodes[index];
-		const a = x - node.x;
-		const b = y - node.y;
-		
-		const c = Math.sqrt(a * a + b * b);
-		
-		if (c < 90) {
-			return node;
-		}
-	}
-	return null;
-}
-
-function drawNodes(ctx, nodes) {
-	for (let index = 0; index < nodes.length; index++) {
-		const node = nodes[index];
-		
-		if (node === selectedNode) {
-			ctx.strokeStyle = "#FF0000";
-		} else {
-			ctx.strokeStyle = "#000000";
-		}
-		
-		ctx.beginPath();
-		ctx.lineWidth = 4;
-		ctx.fillStyle = "#FFFFFF";
-		ctx.arc(node.x, node.y, 40, 0, 2 * Math.PI);
-		ctx.stroke();
-		ctx.fill();
-		
-		if (node === selectedNode) {
-			ctx.fillStyle = "#FF0000";
-		} else {
-			ctx.fillStyle = "#000000";
-		}
-		
-		ctx.font = "30px Arial";
-		ctx.fillText(index, node.x - 5, node.y + 5);
-	}
-}
-
-function drawArcos(ctx, arcos) {
-	for (let index = 0; index < arcos.length; index++) {
-		const arco = arcos[index];
-		ctx.moveTo(arco.node1.x, arco.node1.y);
-		ctx.lineTo(arco.node2.x, arco.node2.y);
-		ctx.strokeStyle = "#000000";
-		ctx.stroke();
-	}
-}
-
+let clickedEl = null;
 
 document.addEventListener("DOMContentLoaded", _ => {
 	/**
@@ -70,24 +14,27 @@ document.addEventListener("DOMContentLoaded", _ => {
 
 	/**
 	 * Layout setup
-		let layout = document.querySelector('#layout')
-		
-		for (let i=0; i <20*7; i++) {
-			let box = document.createElement('div')
-			box.classList.add('box')
-			box.setAttribute('draggable', 'true')
-			box.innerHTML =  `${i}`
-			layout.appendChild(box)
-		}
-		
 	 */
+	let layout = document.querySelector('#layout')
+
+	for (let i=0; i <100; i++) {
+		let box = document.createElement('div')
+		box.classList.add('box')
+		box.setAttribute('draggable', 'true')
+		layout.appendChild(box)
+	}
+
 	let dragSrcEl = null;
-	
+
 	function handleDragStart(e) {
-		this.style.opacity = "0.4";
-		dragSrcEl = this;
-		e.dataTransfer.effectAllowed = "move";
-		e.dataTransfer.setData("text/html", this.innerHTML);
+		if (!isSymbol(this) && this.innerHTML==='')
+			e.preventDefault()
+		else {
+			this.style.opacity = "0.4";
+			dragSrcEl = this;
+			e.dataTransfer.effectAllowed = "move";
+			e.dataTransfer.setData("text/html", this.innerHTML);
+		}
 	}
 	
 	function handleDragOver(e) {
@@ -126,7 +73,8 @@ document.addEventListener("DOMContentLoaded", _ => {
 			item.classList.remove("over");
 		});
 	}
-	
+
+
 	let items = document.querySelectorAll("#layout .box, [id^=\"uml-\"]");
 	items.forEach(function (item) {
 		item.addEventListener("dragstart", handleDragStart, false);
@@ -136,41 +84,6 @@ document.addEventListener("DOMContentLoaded", _ => {
 		item.addEventListener("drop", handleDrop, false);
 		item.addEventListener("dragend", handleDragEnd, false);
 	})
-	
-	
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
-	
-	canvas.addEventListener("click", (e) => {
-		let x = e.clientX - canvas.offsetLeft;
-		let y = e.clientY - canvas.offsetTop;
-		
-		let tempNode = getNodeAt(x, y, nodes);
-		
-		if (selectedNode !== null && tempNode === null) {
-			selectedNode = tempNode;
-			tempNode = null;
-		}
-		
-		if (selectedNode === null) {
-			selectedNode = tempNode;
-			tempNode = null;
-		}
-		
-		if (selectedNode === null) {
-			nodes.push({ x, y });
-		}
-		
-		context.clearRect(0, 0, canvas.width, canvas.height);
-		
-		if (selectedNode !== null && tempNode !== null) {
-			arcos.push({ node1: selectedNode, node2: tempNode });
-			selectedNode = null;
-			tempNode = null;
-		}
-		drawArcos(context, arcos);
-		drawNodes(context, nodes);
-	});
 });
 
 let isSymbol = (element) =>
