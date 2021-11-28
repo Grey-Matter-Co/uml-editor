1c041d0217ea4436bd06ce389ede9c2c541ca4a9
M1ndBlast <gustavpopdzz0@gmail.com>
2021-11-28 02:12:09 -0600

    geeración de código

--- public/js/mind.js
+++ public/js/mind.js
@@ -1,10 +1,19 @@
-let clickedEl = null;
+const CODEBLOCK =
+`#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+/CODE/
+}`
+let selElem = null;
+let rows
 
 document.addEventListener("DOMContentLoaded", _ => {
+	document.querySelector('#textcode').value = CODEBLOCK.replace("/CODE/", '')
+
 	/**
 	 * Toolbar setup
 	 **/
-
 	let popupContainers = document.querySelectorAll('.in-popup-container')
 	for (const popupContainer of popupContainers) {
 
@@ -20,10 +29,16 @@ document.addEventListener("DOMContentLoaded", _ => {
 	 */
 	let layout = document.querySelector('#layout')
 
+	layout.addEventListener('dragend', () => generateCCode())
+	layout.addEventListener('change', () => generateCCode())
+	rows = getComputedStyle(layout).gridTemplateColumns.split(' ').length
 	for (let i=0; i <100; i++) {
 		let box = document.createElement('div')
 		box.classList.add('box')
+		box.setAttribute('col', `${(i%rows) +1}`)
+		box.setAttribute('row', `${parseInt(i/rows)+1}`)
 		box.setAttribute('draggable', 'true')
+		box.addEventListener('click', _ => {if (box.innerHTML==='') rmSelElem()})
 		layout.appendChild(box)
 	}
 
@@ -60,7 +75,7 @@ document.addEventListener("DOMContentLoaded", _ => {
 		if (dragSrcEl && dragSrcEl !== this) {
 			// Layout's element -> Symbol === Nothing
 			if (!isSymbol(dragSrcEl) && isSymbol(this)) {
-				clickedEl = dragSrcEl.firstChild
+				setSelElem(selElem)
 				deleteElement()
 			}
 			// Simbolo -> Layout's element === Add element
@@ -72,14 +87,15 @@ document.addEventListener("DOMContentLoaded", _ => {
 					.then(svgText => {
 						//("focusout",a => alert("lo que quieras"))
 						if (this.innerHTML!=='') {
-							clickedEl = this.firstChild
+							setSelElem(this.firstChild)
 							deleteElement()
 						}
 						this.innerHTML = svgText
-						this.firstChild.classList.add(tipo)
-						this.firstChild.addEventListener('click', function () {
-							clickedEl = this
-						})
+						let svg = this.firstChild
+						svg.classList.add(tipo)
+						svg.querySelector('input')
+							.addEventListener('focus', _ => setSelElem(svg))
+						setSelElem(svg)
 					})
 			}
 			// Layout's element -> Layout's element === Flip elements
@@ -119,30 +135,148 @@ document.addEventListener("DOMContentLoaded", _ => {
 let isSymbol = (element) =>
 	/^uml-/i.test(element.id)
 
+function setSelElem(elem) {
+	for (const elemModer of document.querySelectorAll('.in-mod-elem'))
+		elemModer.classList.remove('disabledbutton')
+	selElem = elem
+}
+
+function rmSelElem() {
+	for (const elemModer of document.querySelectorAll('.in-mod-elem'))
+		elemModer.classList.add('disabledbutton')
+
+	selElem = null;
+}
+
+
+function clearLayout() {
+	for(const box of document.querySelectorAll('.box>svg'))
+		if (box.innerHTML !=='') {
+			setSelElem(box)
+			deleteElement()
+		}
+}
+
+function changeFontsz(input) {
+	if (selElem)
+		selElem.querySelector('input').style.fontSize = `${input.value<1?'1':input.value}rem`
+}
+
 function changeBg(input){
-	if (clickedEl)
-		clickedEl.firstChild.setAttribute('fill', input.value)
+	if (selElem)
+		selElem.firstElementChild.setAttribute('fill', input.value)
 }
 
 function changeBorder(input){
-	if (clickedEl)
-		clickedEl.firstChild.setAttribute('stroke', input.value)
+	if (selElem)
+		selElem.firstElementChild.setAttribute('stroke', input.value)
 }
 
 function deleteElement(){
-	if (clickedEl.classList.contains("start")||clickedEl.classList.contains("end")) {
-		document.querySelector(`[id$=${clickedEl.classList.value}]`).classList.remove('dragged');
-		document.querySelector(`[id$=${clickedEl.classList.value}]`).setAttribute('draggable', 'true');
+	if (selElem.classList.contains("start")||selElem.classList.contains("end")) {
+		document.querySelector(`[id$=${selElem.classList.value}]`).classList.remove('dragged');
+		document.querySelector(`[id$=${selElem.classList.value}]`).setAttribute('draggable', 'true');
 	}
-	if (clickedEl)
-		clickedEl.remove()
-	clickedEl = null;
+	selElem.remove()
+
+	rmSelElem()
 }
 
-function clearLayout() {
-	for(const box of document.querySelectorAll('.box'))
-		if (box.innerHTML !=='') {
-			clickedEl = box.firstChild
-			deleteElement()
-		}
+
+function generateCCode() {
+	let cCode = '',
+		lvl = 1,
+		elem = document.querySelector('svg.start').parentNode
+
+	while (elem) {
+		let tabs = ''
+		for (let i=0; i<lvl; i++)
+			tabs += '\t'
+
+		console.log(`[${elem.elemCoords().toString()}]${elem.elemType()}> ${elem.elemValue()}`)
+
+		cCode += tabs+elem.codeTraslation()
+		elem = elem.nextElem()
+		if (elem)
+			cCode += '\n'
+	}
+	document.querySelector('#textcode').value = CODEBLOCK.replace('/CODE/', cCode)
+}
+
+/**
+ * @returns {String}
+ */
+Node.prototype.elemType = function ()
+	{ return this.querySelector('svg').classList.value }
+
+/**
+ * @returns {String}
+ */
+Node.prototype.elemValue = function ()
+	{ return this.querySelector('input').value }
+
+/**
+ * @returns {Number[]}
+ */
+Node.prototype.elemCoords = function ()
+	{ return [parseInt(this.getAttribute('col')), parseInt(this.getAttribute('row'))] }
+
+/**
+ * @returns {String}
+ */
+Node.prototype.codeTraslation = function () {
+	const val = this.elemValue()
+	switch (this.elemType()) {
+		// Next elem us only back
+		case 'start':
+			return `//\t${val}`
+		case 'process':
+			return `${val};`
+		case 'declaration':
+			return `int ${val} = 0;`
+		case 'input':
+			return `scanf("%d", &${val});`
+		case 'output':
+			return `fprintf("${val}");`
+		case 'condition':
+			return `if("${val}") {}`
+		case 'loop':
+			return `while("${val}") {}`
+		case 'end':
+			return `return 0;\n\t//\t${val}`
+
+	}
+
+}
+
+/**
+ * @returns {Node}
+ */
+Node.prototype.nextElem = function () {
+	switch (this.elemType()) {
+		// Next elem us only back
+		case 'start':
+		case 'process':
+		case 'declaration':
+		case 'input':
+		case 'output':
+			let [x, y] = this.elemCoords(),
+				box = document.querySelector(`#layout > div:nth-child(${x+(y*rows)})`)
+			return box.innerHTML !== ''
+				?box
+				:null
+
+
+		case 'condition':
+			alert("unhandle section")
+			break;
+		case 'loop':
+			alert("unhandle section")
+			break;
+		case 'end':
+			alert("unhandle section")
+			return null;
+
+	}
+
 }
