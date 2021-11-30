47163ffc66c6fdc69f24dbbbc3962ee3ec3ca103
jdann <juanddvalle20@gmail.com>
2021-11-29 23:28:41 -0600

    Changes Danny primera revisión

--- public/js/mind.js
+++ public/js/mind.js
@@ -1,3 +1,57 @@
+/**		Toolbar setup
+ * [] - Adds download btn listener
+ * [] - Adds upload btn listener
+ * [] - Adds delete diagram btn listener
+ * [] - Adds delete item btn listener
+ * [] - Adds custom font sz btn listener
+ * [] - Adds custom SVG bg color btn listener
+ * [] - Adds custom SVG brd color btn listener
+ * [] - Adds popup to custom SVG btn listeners
+ *
+ * 		Layout setup
+ * [] - Loads svg files from server
+ * [] - Sets layout
+ * [] - Sets layout grid's row and column number
+ * [] - Render link lines (cons)
+ * [] - Create Grid's div
+ * [] - Sets Drag & Drop Listener for Grid's divs and Symbols
+ *
+ * 		Drag & Drop logic
+ * 	[] - Drag starting handler
+ * 	[] - Drag enter handler
+ * 	[] - Drag over handler
+ * 	[] - Drag leaving handler
+ * 	[] - Drop handler
+ * 	[] - Drop for Symbol handler
+ * 	[] - Drop for UML's div handler
+ * 	[] - Drag ending handler
+ *
+ *		UML logic
+ * 	[] - updateUI
+ * 	[] - isSymbol
+ * 	[] - getCoords
+ * 	[] - setSelElem
+ * 	[] - rmSelElem
+ * 	[] - setLinkElem
+ * 	[] - rmLinkElem
+ * 	[] - findUMLElem
+ * 	[] - updUMLCoords
+ * 	[] - linkUMLElems
+ * 	[] - drawUMLElemCon
+ * 	[] - getUMLElemConDim
+ * 	[] - getElemSVGByReference
+ *
+ *		Btn Handles
+ * 	[] - popupBtn
+ * 	[] - loadLayout
+ * 	[] - dwlLayout
+ * 	[] - clrLayout
+ * 	[] - changeSz
+ * 	[] - changeBg
+ * 	[] - changeBdr
+ * 	[] - deleteElement
+ **/
+
 const CODEBLOCK =
 `#include <stdio.h>
 #include <stdlib.h>
@@ -5,163 +59,239 @@ const CODEBLOCK =
 int main(int argc, char *argv[]) {
 /CODE/
 }`
+
 /**
- * @type {JSON[]} :clown:
- * @field node {Node}
- * @field type {String}
- * @field connections {Number[]}
+ * @type {[{
+ * 		type: string,
+ * 		ref: {
+ * 		 	x: number,
+ * 		 	y: number
+ * 		 },
+ * 		cons: number[]
+ * }]}
  */
 let flow = []
-let selElem = null;
-let linkElems = {begin: null, end: null};
-let rows
+let SVGs = [],
+	colsN, rowsN
+/**
+ * @type {HTMLDivElement}
+ */
+let layout,
+	selElem,
+	dragSrcEl
+/**
+ * @type {{end: HTMLDivElement, begin: HTMLDivElement}}
+ */
+let linkElems = {begin: undefined, end: undefined}
 
 document.addEventListener("DOMContentLoaded", _ => {
+	//	Loads svg files from server
+	(async _ => {
+		document.querySelector('.loader').classList.remove('d-none')
+		SVGs['condition']	= await fetch(`/uml-svg/condition.svg`).then(file => file.text())
+		SVGs['declaration']	= await fetch(`/uml-svg/declaration.svg`).then(file => file.text())
+		SVGs['end']			= await fetch(`/uml-svg/end.svg`).then(file => file.text())
+		SVGs['input']		= await fetch(`/uml-svg/input.svg`).then(file => file.text())
+		SVGs['output']		= await fetch(`/uml-svg/output.svg`).then(file => file.text())
+		SVGs['process']		= await fetch(`/uml-svg/process.svg`).then(file => file.text())
+		SVGs['start']		= await fetch(`/uml-svg/start.svg`).then(file => file.text())
+		document.querySelector('.loader').classList.add('d-none')
+	})();
+
+	//	Sets layout
+	layout = document.querySelector('#layout')
+	//	Sets layout grid's row number
+	colsN  = getComputedStyle(layout).gridTemplateColumns.split(' ').length
+	rowsN  = getComputedStyle(layout).gridTemplateRows.split(' ').length
 	document.querySelector('#textcode').value = CODEBLOCK.replace("/CODE/", '')
-
-	/**
-	 * Toolbar setup
-	 **/
-	document.querySelector('.bi-download').addEventListener(
-		"click",_=>{
-			downloadLayout();
-		}
-	);
-	let popupContainers = document.querySelectorAll('.in-popup-container')
-	for (const popupContainer of popupContainers) {
-
-		popupContainer.addEventListener('click', function ()
-			{ this.nextSibling.classList.toggle('d-none') })
-
-		popupContainer.nextSibling.firstChild.addEventListener('focusout', function ()
-			{ this.parentNode.classList.toggle('d-none') })
+	//	Adds download btn listener
+	document.querySelector('.bi-download').addEventListener("click", dwlLayout);
+	//	Adds upload btn listener
+	document.querySelector('.bi-upload').addEventListener("click", loadLayout);
+	//	Adds delete diagram btn listener
+	document.querySelector('.bi-trash').addEventListener( "click", clrLayout);
+	//	Adds delete item btn listener
+	document.querySelector('.bi-x-lg').addEventListener( "click", deleteElement);
+	//	Adds custom font sz btn listener
+	document.querySelector('#in-font-sz').addEventListener("input", changeSz);
+	//	Adds custom SVG bg color btn listener
+	document.querySelector('#in-color-bg').addEventListener( "input", changeBg);
+	//	Adds custom SVG brd color btn listener
+	document.querySelector('#in-color-bdr').addEventListener( "input", changeBdr);
+	//	Adds popup to custom SVG btn listeners
+	for (const popupContainer of document.querySelectorAll('.in-popup-container')) {
+		popupContainer.addEventListener('click', popupBtn)
+		popupContainer.nextSibling.firstChild.addEventListener('focusout', popupBtn)
 	}
-
-	/**
-	 * Layout setup
-	 */
-	let layout = document.querySelector('#layout')
-
+	//	Render UML lines (cons)
 	layout.addEventListener('dragend', () => updateUI())
 	layout.addEventListener('change', () => updateUI())
-	rows = getComputedStyle(layout).gridTemplateColumns.split(' ').length
-	for (let i=0; i <100; i++) {
+
+	//	Create Grid's div
+	for (let i=0; i<rowsN*colsN; i++) {
 		let box = document.createElement('div')
 		box.classList.add('box')
-		box.setAttribute('col', `${(i%rows)}`)
-		box.setAttribute('row', `${parseInt(i/rows)}`)
+		box.setAttribute('col', `${(i%colsN)+1}`)
+		box.setAttribute('row', `${Math.floor(i/colsN)+1}`)
 		box.setAttribute('draggable', 'true')
 		box.addEventListener('click', _ => {if (box.innerHTML==='') rmSelElem()})
 		layout.appendChild(box)
 	}
+	// Sets Drag & Drop Listener for Grid's divs and Symbols
+	for (const item of document.querySelectorAll("#layout .box, [id^=uml-]")) {
+		item.addEventListener("dragenter", 	handleDragEnter);
+		item.addEventListener("dragover", 	handleDragOver);
+		item.addEventListener("dragleave", 	handleDragLeave);
+		item.addEventListener("drop", 		handleDrop);
+		item.addEventListener("dragend", 	handleDragEnd);
+		item.addEventListener("dragstart",	handleDragStart);
+	}
 
-	let dragSrcEl = null;
+});
 
-	function handleDragStart(e) {
-		if (!isSymbol(this) && this.innerHTML==='')
-			e.preventDefault()
-		else {
-			this.style.opacity = "0.4";
-			dragSrcEl = this;
-			e.dataTransfer.effectAllowed = "move";
-			e.dataTransfer.setData("text/html", this.innerHTML);
+/**
+ * Drag & Drop
+ */
+//	Drag starting handler
+
+function handleDragStart(e) {
+	if (!isSymbol(this) && this.innerHTML==='')
+		e.preventDefault()
+	else {
+		dragSrcEl = this;
+		dragSrcEl.style.opacity = "0.4";
+		e.dataTransfer.effectAllowed = "move";
+		e.dataTransfer.setData("text/html", dragSrcEl.innerHTML);
+	}
+}
+
+//	Drag enter handler
+function handleDragEnter()
+	{ this.classList.add("over"); }
+
+//	Drag over handler
+function handleDragOver(e) {
+	if (e.preventDefault)
+		e.preventDefault();
+	e.dataTransfer.dropEffect = "move";
+	return false;
+}
+
+//	Drag leaving handler
+function handleDragLeave()
+	{ this.classList.remove("over"); }
+
+//	Drop handler
+function handleDrop(e) {
+	if (e.stopPropagation)
+		e.stopPropagation()
+
+	if (dragSrcEl !== this)
+		if (isSymbol(this))
+			handleDrop_Symbol.call(this, e)
+		else
+			handleDrop_Grid.call(this, e)
+
+	return false;
+}
+//	Drop for Symbol handler
+function handleDrop_Symbol() {
+	// Layout's element -> Symbol === Delete Element
+	if (!isSymbol(dragSrcEl)) {
+		setSelElem(selElem)
+		deleteElement()
+	}
+}
+//	Drop for UML's div handler
+function handleDrop_Grid(e) {
+	// UML's div -> UML's div === Flip elements
+	if (!isSymbol(dragSrcEl)) {
+		let srcCoords = getCoords(dragSrcEl),
+			objCoords = getCoords(this)
+		updUMLCoords(srcCoords, objCoords)
+		if (this.innerHTML!=='')
+			updUMLCoords(objCoords, srcCoords)
+
+		dragSrcEl.innerHTML = this.innerHTML;
+		this.innerHTML = e.dataTransfer.getData("text/html");
+
+	}
+	// Simbolo -> UML's div === Add element
+	else {
+		let umlType = dragSrcEl.id.replace('uml-','');
+
+		if (this.innerHTML!=='') {
+			let idx = findUMLElem(getCoords(this))
+			if (idx>=0)
+				flow[idx].type = umlType
+
+			setSelElem(this)
+			deleteElement()
 		}
-	}
-	
-	function handleDragOver(e) {
-		if (e.preventDefault)
-			e.preventDefault();
-		e.dataTransfer.dropEffect = "move";
-		return false;
-	}
-	
-	function handleDragEnter(e)
-		{ this.classList.add("over"); 	}
-	
-	function handleDragLeave(e)
-		{ this.classList.remove("over"); }
-	
-	function handleDrop(e) {
-		if (e.stopPropagation)
-			e.stopPropagation();
-
-		if (dragSrcEl && dragSrcEl !== this) {
-			// Layout's element -> Symbol === Nothing
-			if (!isSymbol(dragSrcEl) && isSymbol(this)) {
-				setSelElem(selElem)
-				deleteElement()
-			}
-			// Simbolo -> Layout's element === Add element
-			else if (isSymbol(dragSrcEl) && !isSymbol(this)) {
-				let tipo=dragSrcEl.id;
-				tipo=tipo.substring(4);
-				fetch(`/uml-svg/${tipo}.svg`)
-					.then(file => file.text())
-					.then(svgText => {
-						//("focusout",a => alert("lo que quieras"))
-						if (this.innerHTML!=='') {
-							setSelElem(this.firstChild)
-							deleteElement()
-						}
-						this.innerHTML = svgText
-						let svg = this.firstChild
-						svg.classList.add(tipo)
-						svg.querySelector('input')
-							.addEventListener('focus', _ => setSelElem(svg))
-						svg.querySelector('input')
-							.addEventListener('dblclick', _ => setLinkElem(svg))
-						setSelElem(svg)
-					})
-			}
-			// Layout's element -> Layout's element === Flip elements
-			else if (!isSymbol(dragSrcEl) && !isSymbol(this)) {
-				dragSrcEl.innerHTML = this.innerHTML;
-				this.innerHTML = e.dataTransfer.getData("text/html");
-			}
-		}
-		return false;
-	}
-	
-	function handleDragEnd(e) {
-		if(dragSrcEl.id==="uml-start"||dragSrcEl.id==="uml-end") {
+		this.innerHTML = SVGs[umlType]
+		let svg = this.querySelector('svg'),
+			input = this.querySelector('input')
+		svg.classList.add(umlType)
+		input.addEventListener('focus', _ => setSelElem(this))
+		input.addEventListener('dblclick', _ => setLinkElem(this))
+		if(umlType==="start"||umlType==="end") {
+			input.setAttribute('value', umlType==='start' ? 'Inicio' : 'Fin')
 			dragSrcEl.classList.add('dragged');
 			dragSrcEl.setAttribute('draggable', 'false')
 		}
-			this.style.opacity = "";
 
-		items.forEach(item => {
-			item.classList.remove("over");
-		});
-		dragSrcEl = null
-		updateUI()
+		setSelElem(this)
 	}
+}
 
-	let items = document.querySelectorAll("#layout .box, [id^=\"uml-\"]");
-	items.forEach(function (item) {
-		item.addEventListener("dragstart", handleDragStart, false);
-		item.addEventListener("dragenter", handleDragEnter, false);
-		item.addEventListener("dragover", handleDragOver, false);
-		item.addEventListener("dragleave", handleDragLeave, false);
-		item.addEventListener("drop", handleDrop, false);
-		item.addEventListener("dragend", handleDragEnd, false);
-	})
-});
+//	Drag ending handler
+function handleDragEnd() {
+	for (const item of document.querySelectorAll("#layout .box"))
+		item.classList.remove("over");
 
+	this.style.opacity = "";
+	dragSrcEl = null
+	updateUI()
+}
+
+/**
+ * 	UML Logic
+ */
 function updateUI(){
 	generateCCode()
 	for (const divConn of document.querySelectorAll('#layout > .connection'))
 		divConn.remove()
 
 	for (const UMLBegin of flow) {
-		for (const idxEnd of UMLBegin.connections) {
-			drawLink(UMLBegin.node, flow[idxEnd].node)
-		}
+		for (const idxEnd of UMLBegin.cons)
+			drawUMLElemCon(
+				getElemSVGByReference(UMLBegin.ref.x, UMLBegin.ref.y),
+				getElemSVGByReference(flow[idxEnd].ref.x, flow[idxEnd].ref.y)
+			)
 	}
 }
 
+/**
+ * @param element {HTMLElement}
+ * @return {boolean}
+ */
 const isSymbol = element =>
 	/^uml-/i.test(element.id)
 
+/**
+ * @param UMLDiv {HTMLDivElement}
+ * @returns {{x: number, y: number}}
+ */
+function getCoords(UMLDiv) {
+	return {
+		x: parseInt(UMLDiv.getAttribute('col')),
+		y: parseInt(UMLDiv.getAttribute('row'))
+	}
+}
+
+/**
+ * @param elem {HTMLDivElement}
+ */
 const setSelElem = elem => {
 	for (const elemModer of document.querySelectorAll('.in-mod-elem'))
 		elemModer.classList.remove('disabledbutton')
@@ -185,70 +315,74 @@ const setLinkElem = (elem) => {
 		rmLinkElem()
 	}
 }
-const rmLinkElem = () => {
-	linkElems = { begin: null, end: null }
+
+const rmLinkElem = () =>
+	{ linkElems = {begin: undefined, end: undefined} }
+
+/**
+ * @param coords {{x: number, y: number}}
+ * @returns {number}
+ */
+const findUMLElem = (coords) => {
+	let idx = -1
+	flow.some((umlElem, i) => {
+		if (umlElem.ref.x===coords.x && umlElem.ref.y===coords.y) {
+			idx = i
+			return true
+		}
+		else
+			return false
+	})
+	return idx
 }
+
+/**
+ * @param oldCoords {{x: number, y: number}}
+ * @param newCoords {{x: number, y: number}}
+ */
+const updUMLCoords = (oldCoords, newCoords) =>
+	{ flow[findUMLElem(oldCoords)].ref = newCoords }
 
 const mkLink = () => {
 	let iBegin, iEnd,
 		isLinked;
 	// Adds begin element
-	isLinked = flow.some((UMLElem, idx) => {
-		if (UMLElem.node === linkElems.begin) {
-			iBegin = idx;
-			return true
-		} else
-			return false
-	})
-	if (!isLinked)
+	if (iBegin<0)
 		iBegin = flow.push({
-			node: linkElems.begin,
-			type: linkElems.begin.parentNode.elemType(),
-			connections: []
+			ref: getCoords(linkElems.begin),
+			type: linkElems.begin.elemType(),
+			cons: []
 		})-1
 	// Adds end element
-	isLinked = flow.some((UMLElem, idx) => {
-		if (UMLElem.node === linkElems.end)
-		{ iEnd = idx; return true }
-		else
-			return false
-	})
-	if (!isLinked)
+	if (iEnd<0)
 		iEnd = flow.push({
-			node: linkElems.end,
-			type: linkElems.end.parentNode.elemType(),
-			connections: []
+			ref: getCoords(linkElems.end),
+			type: linkElems.end.elemType(),
+			cons: []
 		})-1
 
-	flow[iBegin].connections.push(iEnd)
+
+	if(flow[iBegin].cons.every(conn => conn!==iEnd))
+		flow[iBegin].cons.push(iEnd)
+
+	// console.log("flow: "+JSON.stringify(flow, null, 4))
 	updateUI()
-	console.log("flow: "+JSON.stringify(flow, null, 4))
 }
 
-const drawLink = (UMLBegin, UMLEnd) => {
-	const offsetX = document.querySelector("body > main > div").getBoundingClientRect().width
-	const offsetY = document.querySelector("body > div.title").getBoundingClientRect().height
+/**
+ * @param SVGBegin	{SVGElement}
+ * @param SVGEnd	{SVGElement}
+ */
+const drawUMLElemCon = (SVGBegin, SVGEnd) => {
+	let beginCoords = SVGBegin.getBoundingClientRect(),
+		endCoords   = SVGEnd.getBoundingClientRect();
 
-	let x1, y1, x2, y2, beginCoords, endCoords;
-	beginCoords = UMLBegin.getBoundingClientRect()
-	endCoords = UMLEnd.getBoundingClientRect()
-	if (beginCoords.left<endCoords.left) {
-		x1 = (beginCoords.left-offsetX)+(beginCoords.width/2)-10
-		x2 = (endCoords.left-offsetX)+(endCoords.width/2)-10
-	}
-	else {
-		x1 = (endCoords.left-offsetX)+(endCoords.width/2)-10
-		x2 = (beginCoords.left-offsetX)+(beginCoords.width/2)-10
-	}
-	if (beginCoords.top<endCoords.top) {
-		y1 = (beginCoords.top-offsetY)+beginCoords.height-10
-		y2 = (endCoords.top-offsetY)
-	}
-	else {
-		y1 = (endCoords.top-offsetY)+endCoords.height-10
-		y2 = (beginCoords.top-offsetY)
-	}
-
+	let [x1, x2, y1, y2, xFlag, yFlag] = getUMLElemConDim(beginCoords, endCoords),
+		xF = xFlag?1:-1,
+		yF = yFlag?1:-1,
+		strk = 4,
+		long = 20,
+		ymid = endCoords.height/2
 
 
 	let createRow = document.createElement('div')
@@ -257,16 +391,75 @@ const drawLink = (UMLBegin, UMLEnd) => {
 	createRow.style.left=x1+"px"
 	createRow.style.top=y1+"px"
 
-	createRow.innerHTML = '<svg width="'+(Math.abs(x2-x1)+20)+'" height="'+(Math.abs(y2-y1))+'" xmlns="http://www.w3.org/2000/svg">' +
-		'<g>' +
-			'<line id="svg_2" stroke="black" fill="black" stroke-width="5.5" x1="0" y1="0" x2="'+Math.abs(x2-x1)+'" y2="'+Math.abs(y2-y1)+'" stroke-linejoin="null" stroke-linecap="null"></line>' +
-			'<path id="svg_9" transform="rotate(89.3603 152.594 14.6817)" fill="black" stroke-width="1.5" d="m142.14651,24.21188l10.4478,-19.06026l10.44754,19.06026l-20.89534,0z" stroke="black"></path>' +
-		'</g>' +
-		'</svg>';
-	document.querySelector("#layout" ).appendChild(createRow)
-
+	// <path stroke="black" fill="none"  stroke-width=${strk} d="m${(xFlag?(strk/2):x2+strk)+7.25},${yFlag?0:y2+10} v${yF*y2/2},0  h${xF*Math.abs(x2)+(xFlag?0:5)},0 v${yF*y2/2},0"></path>
+	// <path stroke="black" fill="black" stroke-width="1.5"   d="m0,0 l-10,-20 l20,0 z" transform="rotate(${yFlag?'0':'180 10 5'}) translate(${(xFlag?yF*x2:0)+10} ${(yFlag?y2:0)+10})"></path>
+	console.log(`xFlag ${xFlag} | yFlag ${yFlag}`)
+	createRow.innerHTML =
+`<svg width=${(x2?x2:long/2)+long/2} height=${y2+(yFlag?0:ymid)} style="transform: translateX(${xFlag?-strk/2:-(long-strk)/2}px);" xmlns="http://www.w3.org/2000/svg">
+	<!--<path stroke="black" fill="none"  stroke-width=${strk} d="m${xor?strk/2:(x2+(long-strk)/2)},${yFlag?0:ymid} v${y2/2+ymid},0 h${(xor?1:-1)*x2},0 v${y2/2},0"></path>-->
+	<path stroke="black" fill="black" stroke-width="1.5"   d="m0,0 l${-long/2},${-long} l${long},0 z" transform="rotate(0) translate(${xF*(Math.abs(x2-long/2)+xFlag?0long/2)} ${y2})"></path
+</svg>`;
+	layout.appendChild(createRow)
 }
 
+/**
+ -(* 	(x1,y1)--------------)
+ * 	   |				|
+ * 	   |				|
+ * 	   |		<3		|
+ * 	   |				|
+ * 	   |				|
+ * 	   --------------(x2,y2)
+ * 	   @param domRect1 {DOMRect}
+ * 	   @param domRect2 {DOMRect}
+ * 	   @returns {number[4]}
+ */
+const getUMLElemConDim = (domRect1, domRect2) => () {
+	let x1, x2, y1, y2, xFlag, yFlag,
+		toolsDiv = document.querySelector("body > main > div").getBoundingClientRect(),
+		headerDiv = document.querySelector("body > div.title").getBoundingClientRect();
+
+	const offsetX = toolsDiv.width + toolsDiv.left,
+		  offsetY = headerDiv.height + headerDiv.top;
+
+	// X axis
+	if (domRect1.left <= domRect2.left) {
+		x1  = domRect1.left + (domRect1.width/2)
+		x2  = domRect2.left + (domRect2.width/2) - x1
+		xFlag = true
+	}
+	else {
+		x1  = domRect2.left + (domRect2.width/2)
+		x2  = domRect1.left + (domRect1.width/2) - x1
+		xFlag = false
+	}
+
+	// Y axis
+	if (domRect1.top <= domRect2.top) {
+		y1  = domRect1.top + (domRect1.height/2)
+		y2  = domRect2.top						 - y1-20
+		yFlag = true
+	}
+	else {
+		y1  = domRect2.top + (domRect2.height/2)
+		y2  = domRect1.top						 - y1-20
+		yFlag = false
+	}
+
+	x1 -= offsetX
+	y1 -= offsetY
+
+	console.log(`x1=${x1}, x2=${x2}, y1=${y1}, y2=${y2}`)
+	return [x1, x2, y1, y2, xFlag, yFlag]
+}
+
+/**
+ * @param x: number
+ * @param y: number
+ * @return {SVGElement}
+ */
+const getElemSVGByReference = (x, y) =>
+	document.querySelector(`#layout > div:nth-child(${x+(--y*colsN)}) > svg`)
 
 
 const downloadLayout  = _ => {
@@ -279,9 +472,10 @@ const downloadLayout  = _ => {
 	document.body.appendChild(a);
 	a.click();
 }
-
-const clearLayout = _ => {
-	for(const box of document.querySelectorAll('.box>svg'))
+function clrLayout() {
+    for(const connDiv of document.querySelectorAll('#layout>.connection'))
+        connDiv.remove()
+	for(const box of document.querySelectorAll('#layout>.box'))
 		if (box.innerHTML !=='') {
 			setSelElem(box)
 			deleteElement()
@@ -412,7 +606,7 @@ Node.prototype.nextElem = function () {
 		case 'input':
 		case 'output': {
 			let [x, y] = this.elemCoords(),
-				box = document.querySelector(`#layout > div:nth-child(${++x+(++y*rows)})`)
+				box = document.querySelector(`#layout > div:nth-child(${++x+(++y*colsN)})`)
 			return box.innerHTML !== ''
 				?box
 				:null
