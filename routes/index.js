fd37385ea4486eed6b4f6f0cc35b13ca0c030534
M1ndBlast <gustavopdzz0@gmail.com>
2021-12-11 00:06:50 -0600

    turas para portada

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'Flowchart' });
 });
 
+router.get('/editor', function(req, res) {
+  res.render('editor', { title: 'Flowchart' });
+});
+
 module.exports = router;
+/**
+ * wenas. tienes problemas de internet? o soy yo
+ * chale JAJAJA
+ * ptm
+ * hago push
+ *
+ * tú jajajaj me saca a cada rato
+ */
